fd6e56df478608bd27f43e1ab72b93b50d0f5432
Govind S R <64773624+theinfinox@users.noreply.github.com>
2021-08-30 14:27:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port =(process.env.PORT || 3000);
+const port =(process.env.PORT || 8080);
 const scrapper = require('./scrapper');
 const fs = require('fs');
 const dataPath = 'notifications.json';
